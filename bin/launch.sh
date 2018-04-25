d1d7a38798552550ac1e3b7b48de32b5cdd86e63
Tommy Tan <tommy.tan@west.com>
2018-04-25 16:21:40 +0800

    Change nginx conf path

--- bin/launch.sh
+++ bin/launch.sh
@@ -5,9 +5,9 @@ pwd
 ls -la $HOME
 
 # create nginx conf file with PORT and HOME directory from cloudfoundry environment variables
-#mv $HOME/nginx/conf/nginx.conf $HOME/nginx/conf/nginx.conf.original
-#sed "s|\(^\s*listen\s*\)80|\1$PORT|" $HOME/nginx/conf/nginx.conf.original > $HOME/nginx/conf/nginx.conf
-#sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/nginx/conf/nginx.conf
+mv $HOME/nginx/conf/nginx.conf $HOME/nginx/conf/nginx.conf.original
+sed "s|\(^\s*listen\s*\)80|\1$PORT|" $HOME/nginx/conf/nginx.conf.original > $HOME/nginx/conf/nginx.conf
+sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/nginx/conf/nginx.conf
 
 # debug output
 cat $HOME/nginx/conf/nginx.conf
