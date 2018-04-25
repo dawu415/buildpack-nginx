8c0db89365eed5cbb6717ff1d2c14ad6c845e9c3
Tommy Tan <tommy.tan@west.com>
2018-04-25 17:08:45 +0800

    Change nginx work dir.

--- bin/launch.sh
+++ bin/launch.sh
@@ -6,7 +6,7 @@ ls -la $HOME
 
 # create nginx conf file with PORT and HOME directory from cloudfoundry environment variables
 mv $HOME/nginx/conf/nginx.conf $HOME/nginx/conf/nginx.conf.original
-sed "s|\(^\s*listen\s*\)80|8086|" $HOME/nginx/conf/nginx.conf.original > $HOME/nginx/conf/nginx.conf
+sed "s|\(^\s*listen\s*\)80|\1$PORT|" $HOME/nginx/conf/nginx.conf.original > $HOME/nginx/conf/nginx.conf
 sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/nginx/conf/nginx.conf
 
 # debug output
@@ -14,7 +14,7 @@ cat $HOME/nginx/conf/nginx.conf
 
 #create nginx logs folder
 echo "Create Nginx Log folder"
-mkdir -p /home/vcap/nginx/logs
+mkdir -p /home/vcap/app/nginx/logs
 
 echo "Start nginx web server: " + $HOME/nginx/sbin/nginx
 # start nginx web server
