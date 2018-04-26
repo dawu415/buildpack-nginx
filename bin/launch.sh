909eed39a9712f77010e62b1199eb538521520fb
Tommy Tan <tommy.tan@west.com>
2018-04-26 16:28:20 +0800

    PORT

--- bin/launch.sh
+++ bin/launch.sh
@@ -6,7 +6,7 @@ ls -la $HOME
 
 # create nginx conf file with PORT and HOME directory from cloudfoundry environment variables
 mv $HOME/nginx/conf/nginx.conf $HOME/nginx/conf/nginx.conf.original
-sed "s|\(^\s*listen\s*\)80|\1$Port|" $HOME/nginx/conf/nginx.conf.original > $HOME/nginx/conf/nginx.conf
+sed "s|\(^\s*listen\s*\)80|\1$PORT|" $HOME/nginx/conf/nginx.conf.original > $HOME/nginx/conf/nginx.conf
 sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/nginx/conf/nginx.conf
 
 # debug output
