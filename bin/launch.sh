f71740d742b0f1d76d149139209c6758bc60b91c
Tommy Tan <tommy.tan@west.com>
2018-04-27 11:47:51 +0800

    Add -t

--- bin/launch.sh
+++ bin/launch.sh
@@ -14,8 +14,8 @@ sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/nginx/conf/nginx.conf
 
 echo "Start nginx web server: "
 # start nginx web server
-$HOME/nginx/bin/nginx -c $HOME/nginx/conf/nginx.conf -p $HOME/nginx
+$HOME/nginx/bin/nginx -t -c $HOME/nginx/conf/nginx.conf -p $HOME/nginx
 
 cat /home/vcap/app/nginx/logs/error.log
 ls -la $HOME/public
-#echo "Nginx Started" + $HOME/nginx/bin/nginx -c $HOME/nginx/conf/nginx.conf -p $HOME/nginx
+#echo "Nginx Started" + $HOcd,ME/nginx/bin/nginx -c $HOME/nginx/conf/nginx.conf -p $HOME/nginx
