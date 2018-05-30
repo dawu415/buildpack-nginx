d7a9eabeb0cf674ae6020ead7d3372ad919c1077
Tommy Tan <tommy.tan@west.com>
2018-05-30 11:28:49 -0400

    Change the PORT path

--- bin/launch.sh
+++ bin/launch.sh
@@ -2,12 +2,12 @@
 
 # debug output
 pwd
-ls -la $HOME/app/public
+ls -la $HOME/public
 
 # create nginx conf file with PORT and HOME directory from cloudfoundry environment variables
 #mv $HOME/public/nginx.conf $HOME/nginx/public/nginx.conf.original
-sed -i "s|\(^\s*listen\s*\)8080|\1$PORT|" $HOME/app/public/sites-available/default
-sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/app/public/sites-available/default
+sed -i "s|\(^\s*listen\s*\)8080|\1$PORT|" $HOME/public/sites-available/default
+sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/public/sites-available/default
 
 # debug output
 #cat $HOME/nginx/conf/nginx.conf
@@ -15,8 +15,8 @@ sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/app/public/sites-available/
 echo "Start nginx web server: "
 # start nginx web server
 #$HOME/nginx/bin/nginx -t -c $HOME/nginx/conf/nginx.conf -p $HOME/nginx
-$HOME/nginx/bin/nginx -t -c $HOME/app/public/nginx.conf -p $HOME/nginx
-$HOME/nginx/bin/nginx -c $HOME/app/public/nginx.conf -p $HOME/nginx
+$HOME/nginx/bin/nginx -t -c $HOME/public/nginx.conf -p $HOME/nginx
+$HOME/nginx/bin/nginx -c $HOME/public/nginx.conf -p $HOME/nginx
 
-ls -la $HOME/app/public
+ls -la $HOME/public
 #echo "Nginx Started" + $HOcd,ME/nginx/bin/nginx -c $HOME/nginx/conf/nginx.conf -p $HOME/nginx
