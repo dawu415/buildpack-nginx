8aa4c854572632ddfe2d886701640d66ad53d9cf
Tommy Tan <tommy.tan@west.com>
2018-05-30 11:36:22 -0400

    Replace port path

--- bin/launch.sh
+++ bin/launch.sh
@@ -6,8 +6,8 @@ ls -la $HOME/public
 
 # create nginx conf file with PORT and HOME directory from cloudfoundry environment variables
 #mv $HOME/public/nginx.conf $HOME/nginx/public/nginx.conf.original
-sed -i "s|\(^\s*listen\s*\)8080|\1$PORT|" $HOME/public/sites-available/default
-sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/public/sites-available/default
+#sed -i "s|\(^\s*listen\s*\)8080|\1$PORT|" $HOME/public/sites-available/default
+#sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/public/sites-available/default
 
 # debug output
 #cat $HOME/nginx/conf/nginx.conf
