e80efca1c5de994ec1eb53d40c31f911c5045a43
Tommy Tan <tommy.tan@west.com>
2018-05-30 11:06:39 -0400

    Use environment variable port

--- bin/launch.sh
+++ bin/launch.sh
@@ -2,12 +2,12 @@
 
 # debug output
 pwd
-ls -la $HOME
+ls -la $HOME/public
 
 # create nginx conf file with PORT and HOME directory from cloudfoundry environment variables
-mv $HOME/nginx/conf/nginx.conf $HOME/nginx/conf/nginx.conf.original
-sed "s|\(^\s*listen\s*\)80|\1$PORT|" $HOME/nginx/conf/nginx.conf.original > $HOME/nginx/conf/nginx.conf
-sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/nginx/conf/nginx.conf
+#mv $HOME/public/nginx.conf $HOME/nginx/public/nginx.conf.original
+sed -i "s|\(^\s*listen\s*\)8080|\1$PORT|" $HOME/public/sites-available/default
+sed -i "s|\(^\s*root\s*\)html|\1$HOME/public|" $HOME/public/sites-available/default
 
 # debug output
 #cat $HOME/nginx/conf/nginx.conf
@@ -18,6 +18,5 @@ echo "Start nginx web server: "
 $HOME/nginx/bin/nginx -t -c $HOME/public/nginx.conf -p $HOME/nginx
 $HOME/nginx/bin/nginx -c $HOME/public/nginx.conf -p $HOME/nginx
 
-cat /home/vcap/app/nginx/logs/error.log
 ls -la $HOME/public
 #echo "Nginx Started" + $HOcd,ME/nginx/bin/nginx -c $HOME/nginx/conf/nginx.conf -p $HOME/nginx
